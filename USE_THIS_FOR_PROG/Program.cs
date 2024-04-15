3865b7c2e20fcd6f74da3e7e81b9d50c5349b18c
ganeef <ganee@DESKTOP-POCL62K>
2024-04-15 10:28:55 +0200

    added more comments with a better desing to the application

--- USE_THIS_FOR_PROG/Program.cs
+++ USE_THIS_FOR_PROG/Program.cs
@@ -10,85 +10,89 @@ namespace USE_THIS_FOR_PROG
     {
         static void Main(string[] args)
         {
-            bool addAnotherRecipe = true;
-            while (addAnotherRecipe)
+            //USER MUST ENTER RECIPE NAME
+            Console.WriteLine("Enter Recipe Name:");
+            string recipeName = Console.ReadLine();
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Recipe Name: " + recipeName);
+            Console.WriteLine("-------------------");
+            //USER MUST ENTER THE AMOUNT OF INGREDIENTS
+            Console.WriteLine();
+            Console.WriteLine("How many ingredients?");
+            Console.WriteLine("-------------------");
+            int ingredientNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------");
+
+            List<string> ingredients = new List<string>();
+            for (int i = 0; i < ingredientNumber; i++)
             {
-                //ASKS USER TO ENTER NAME
-                Console.WriteLine("Enter Recipe Name:");
-                string recipeName = Console.ReadLine();
-                //ASKS USER HOW MANY INGREDIENTS
-                Console.WriteLine("How many ingredients?");
-                int ingredientNumber = int.Parse(Console.ReadLine());
-
-                List<string> ingredients = new List<string>();
-                for (int i = 0; i < ingredientNumber; i++)
-                {
-                    //ENTER THE AMOUNT OF INGREDIENTS YOU ENTERED
-                    Console.Write($"Enter Ingredient Name {i + 1}: ");
-                    ingredients.Add(Console.ReadLine());
-                }
-                //ENTER THE MEASUREMENTS OF RECIPE
-                Console.WriteLine("Enter recipe measurements (e.g., 1 cup, 250 grams, etc.):");
-                string measureAmount = Console.ReadLine();
-                Console.WriteLine("Measurements: " + measureAmount);
-
-                double amount = double.Parse(measureAmount);
-                //ENTER MEASUREMENT SCALING-0.5 only works with the number 1 for some reason
-                Console.WriteLine("Enter scaling factor (e.g., 0.5, 2, 3):");
-                double scalingAmount = double.Parse(Console.ReadLine());
-                //SCALING AMOUNTS
-                Console.WriteLine($"Scaled by {scalingAmount * 0.5}: {amount * (scalingAmount * 0.5)}");
-                Console.WriteLine($"Scaled by {scalingAmount}: {amount * scalingAmount}");
-                Console.WriteLine($"Scaled by {scalingAmount * 3}: {amount * (scalingAmount * 3)}");
-                //RECIPE STEPS
-                Console.WriteLine("How many steps?");
-                int howManySteps = int.Parse(Console.ReadLine());
-
-                string[] instructions = new string[howManySteps];
-                Console.WriteLine("Recipe Instructions:");
-                for (int i = 0; i < howManySteps; i++)
-                {
-                    Console.WriteLine("Step " + (i + 1) + ":");
-                    instructions[i] = Console.ReadLine();
-                }
-                //INSTRUCTIONS AMOUNT DEPENDS ON NUMBER ENTERED INTO THE STEPS
-                Console.WriteLine("Instructions:");
-                foreach (var step in instructions)
-                {
-                    Console.WriteLine(step);
-                }
-                //RECIPE SUMMARY
-                Console.WriteLine("--- Recipe Summary ---");
-                Console.WriteLine("Recipe Name: " + recipeName);
-                Console.WriteLine("Ingredients:");
-                foreach (var ingredient in ingredients)
-                {
-                    Console.WriteLine("- " + ingredient);
-                }
-                Console.WriteLine("Steps:");
-                foreach (var step in instructions)
-                {
-                    Console.WriteLine("- " + step);
-                }
-                //USER CAN ADD NEW RECIPE OR EXIT APPLICATION
-                Console.WriteLine("Do you want to add another recipe? (yes/no)");
-                string choice = Console.ReadLine().ToLower();
-                if (choice != "yes")
-                {
-                    //DO YOU WANT TO EXIT OR ADD ANOTHER RECIPE?
-                    addAnotherRecipe = false;
-                    Console.WriteLine("Press on keybaord to exit application........");
-                    string endChoice = Console.ReadLine().ToLower();
-                    if (endChoice == "yes")
-                    {
-                        //USER EXITS
-                        Console.WriteLine("exited....");
-                        break;
-                    }
-                }
+                //USER MUST NOW ENTER THE NAME OF ENGREDIENTS
+                Console.Write($"Enter Ingredient Name:{i + 1}: ");
+                ingredients.Add(Console.ReadLine());
+                Console.WriteLine();
             }
+            //USER MUST NOW ENTER THE MEASUREMONTS OF THESE ENGREDIENTS
+            Console.WriteLine("Enter recipe measurements (e.g., 1 cup, 250 grams, etc.):");
+            string measureAmount = Console.ReadLine();
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Measurements: " + measureAmount);
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine();
+            double amount = double.Parse(measureAmount);
+            //SCALE THE NEASUREMENT AMMOUNT-0.5 only works when 1 is typed and not 0.5
+            Console.WriteLine("Enter scaling factor (e.g., 0.5, 2, 3):");
+            double scalingAmount = double.Parse(Console.ReadLine());
+
+            //SCALING
+            Console.WriteLine($"Scaled by {scalingAmount * 0.5}: {amount * (scalingAmount * 0.5)}");
+            Console.WriteLine($"Scaled by {scalingAmount}: {amount * scalingAmount}");
+            Console.WriteLine($"Scaled by {scalingAmount * 3}: {amount * (scalingAmount * 3)}");
+
+          //USER MUST ENTER THE AMOUNT OF STEPS
+            Console.WriteLine("How many steps?");
+            Console.WriteLine("-------------------");
+            int howManySteps = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------");
+
+            string[] instructions = new string[howManySteps];
+            //USER MUST ENTER THE INSTRUCTION OF HOW THE RECIPE MUST BE MADE
+            Console.WriteLine("Recipe Instructions:");
+            for (int i = 0; i < howManySteps; i++)
+            {
+                Console.WriteLine("Step " + (i + 1) + ":");
+                instructions[i] = Console.ReadLine();
+            }
+            //DISPLAY THOSE INSTRUCTIONS
+            Console.WriteLine("Instructions:");
+            foreach (var step in instructions)
+            {
+                Console.WriteLine(step);
+            }
+            //RECIPE SUMMARY
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Recipe Summary");
+            Console.WriteLine("Recipe Name: " + recipeName);
+            Console.WriteLine("Ingredients:");
+            foreach (var ingredient in ingredients)
+            {
+                Console.WriteLine("- " + ingredient);
+            }
+            Console.WriteLine("How To Make:");
+            foreach (var step in instructions)
+            {
+                Console.WriteLine("- " + step);
+                Console.WriteLine("-------------------");
+
+            }
+
+            Console.ReadLine();
+           
         }
     }
 }
+
+
+ 
 
 
